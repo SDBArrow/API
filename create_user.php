07a76f598b99730551d2a01020e668831174a458
YangTzuHung <j25889651556@gmail.com>
2022-03-02 02:41:35 +0800

    202203020241

--- create_user.php
+++ create_user.php
@@ -7,7 +7,7 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // files needed to connect to database
-include_once 'config/database.php';
+include_once 'config/connect.php';
 include_once 'objects/user.php';
  
 // get database connection
