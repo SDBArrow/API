c29d12cf64b8f3857106bcdb25c2f6f1e886f628
YangTzuHung <j25889651556@gmail.com>
2022-03-03 00:58:49 +0800

    202203030058

--- test.php
+++ test.php
@@ -7,7 +7,7 @@ include_once 'libs/php-jwt-main/src/SignatureInvalidException.php';
 include_once 'libs/php-jwt-main/src/JWT.php';
 
 use \Firebase\JWT\JWT;
-use \Firebase\JWT\Key;
+use Firebase\JWT\Key;
 
 $jwt="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NDYyNDAwODUsImV4cCI6MTY0NjI0MzY4NSwiaXNzIjoiaHR0cDovL3NpZ24tcmVnaXN0ZXIuaGVyb2t1YXBwLmNvbS8iLCJkYXRhIjp7ImlkIjo4NCwiZmlyc3RuYW1lIjoiTWlrZSIsImxhc3RuYW1lIjoiRGFsaXNheSIsImVtYWlsIjoibWlrZUBjb2Rlb2ZhbmluamEuY29tIn19.4ttrdcmvjoNmmZWs1f8kdJORu6Wo1XLHiUj3abg4gYc";
 
