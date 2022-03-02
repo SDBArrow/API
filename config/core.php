19ca52436d7da6fda3cc605a5f81e80a2ffd3451
YangTzuHung <j25889651556@gmail.com>
2022-03-02 23:25:21 +0800

    202203022325

--- config/core.php
+++ config/core.php
@@ -3,11 +3,11 @@
 error_reporting(E_ALL);
  
 // set your default time-zone
-date_default_timezone_set('Asia/Manila');
+date_default_timezone_set('Asia/Taipei');
  
 // variables used for jwt
 $key = "example_key";
 $issued_at = time();
 $expiration_time = $issued_at + (60 * 60); // valid for 1 hour
-$issuer = "http://localhost/CodeOfaNinja/RestApiAuthLevel1/";
+$issuer = "http://sign-register.herokuapp.com/";
 ?>
