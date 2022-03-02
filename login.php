7ef9d26fe2751f5b1174cbb970aa212031d8d1be
YangTzuHung <j25889651556@gmail.com>
2022-03-02 23:07:30 +0800

    202203022307_JWT

--- login.php
+++ login.php
@@ -24,4 +24,48 @@ $data = json_decode(file_get_contents("php://input"));
 $user->email = $data->email;
 $email_exists = $user->emailExists();
  
-// files for jwt will be here
+// generate json web token
+include_once 'config/core.php';
+include_once 'libs/php-jwt-main/src/BeforeValidException.php';
+include_once 'libs/php-jwt-main/src/ExpiredException.php';
+include_once 'libs/php-jwt-main/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-main/src/JWT.php';
+use \Firebase\JWT\JWT;
+ 
+// check if email exists and if password is correct
+if($email_exists && password_verify($data->password, $user->password)){
+ 
+    $token = array(
+       "iat" => $issued_at,
+       "exp" => $expiration_time,
+       "iss" => $issuer,
+       "data" => array(
+           "id" => $user->id,
+           "firstname" => $user->firstname,
+           "lastname" => $user->lastname,
+           "email" => $user->email
+       )
+    );
+ 
+    // set response code
+    http_response_code(200);
+ 
+    // generate jwt
+    $jwt = JWT::encode($token, $key);
+    echo json_encode(
+            array(
+                "message" => "Successful login.",
+                "jwt" => $jwt
+            )
+        );
+ 
+}// login failed
+else{
+ 
+    // set response code
+    http_response_code(401);
+ 
+    // tell the user login failed
+    echo json_encode(array("message" => "Login failed."));
+}
+?>
