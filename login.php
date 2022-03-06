a07b675acf5a20eacb5b034debfcd9eb8b9ed2cd
YangTzuHung <j25889651556@gmail.com>
2022-03-06 19:50:45 +0800

    202203061950

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 // 限制接收數據的來源以及類型
-header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
