b037485e0c2e83409a90f4729b3669e1e6285816
YangTzuHung <j25889651556@gmail.com>
2022-03-06 23:42:53 +0800

    202203062342

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
 // 限制接收數據的來源以及類型
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: *");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
