5527b76e164492a17427c01b6f043c1ff8a3bd62
YangTzuHung <j25889651556@gmail.com>
2022-03-07 00:07:38 +0800

    202203070007

--- login.php
+++ login.php
@@ -1,14 +1,15 @@
 <?php
 // 限制接收數據的來源以及類型
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://testrosagv.herokuapp.com/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
+
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == "OPTIONS") {
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: https://testrosagv.herokuapp.com/');
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
     header("HTTP/1.1 200 OK");
     die();
