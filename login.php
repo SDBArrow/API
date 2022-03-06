148480a6abea14455f2d39a47d291e3dd8b41896
YangTzuHung <j25889651556@gmail.com>
2022-03-07 00:03:56 +0800

    202203070003

--- login.php
+++ login.php
@@ -6,73 +6,80 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
- 
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("HTTP/1.1 200 OK");
+    die();
+}
+
 // files needed to connect to database
 include_once 'config/DBconnect.php';
 include_once 'objects/user.php';
- 
+
 // get database connection
 $database = new Database();
 $db = $database->getConnection();
- 
+
 // instantiate user object
 $user = new User($db);
- 
+
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
- 
+
 // set product property values
 $user->email = $data->email;
 $email_exists = $user->emailExists();
- 
+
 // 引入生成 json web token 的library
 include_once 'config/core.php';
 include_once 'libs/php-jwt-main/src/BeforeValidException.php';
 include_once 'libs/php-jwt-main/src/ExpiredException.php';
 include_once 'libs/php-jwt-main/src/SignatureInvalidException.php';
 include_once 'libs/php-jwt-main/src/JWT.php';
+
 use \Firebase\JWT\JWT;
- 
+
 
 // 確認email是否存在 密碼是否正確
-if($email_exists && password_verify($data->password, $user->password)){
+if ($email_exists && password_verify($data->password, $user->password)) {
 
     $token = array(
-       "iat" => $issued_at,
-       "exp" => $expiration_time,
-       "iss" => $issuer,
-       "data" => array(
-           "id" => $user->id,
-           "firstname" => $user->firstname,
-           "lastname" => $user->lastname,
-           "email" => $user->email
-       ),
+        "iat" => $issued_at,
+        "exp" => $expiration_time,
+        "iss" => $issuer,
+        "data" => array(
+            "id" => $user->id,
+            "firstname" => $user->firstname,
+            "lastname" => $user->lastname,
+            "email" => $user->email
+        ),
     );
- 
+
     // set response code
     http_response_code(200);
- 
+
     // generate jwt
     $jwt = JWT::encode($token, $key, 'HS256');
     echo json_encode(
-            array(
-                "message" => "Successful login.",
-                "jwt" => $jwt,
-                "result" => 'ok',
-            )
-        );
-}// login failed
-else{
- 
+        array(
+            "message" => "Successful login.",
+            "jwt" => $jwt,
+            "result" => 'ok',
+        )
+    );
+} // login failed
+else {
+
     // set response code
     http_response_code(401);
- 
+
     // tell the user login failed
     echo json_encode(
         array(
             "message" => "Login failed.",
-            "result" => 'ok',            
+            "result" => 'ok',
         )
     );
 }
-?>
