4a3112ff4cd82192612ec79dc37ab05509719f04
YangTzuHung <j25889651556@gmail.com>
2022-03-06 21:56:02 +0800

    202203062156

--- login.php
+++ login.php
@@ -5,6 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Credentials: true");
  
 // files needed to connect to database
 include_once 'config/DBconnect.php';
