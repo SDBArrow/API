be17a69cd876c5258afa0a55ded02d20cde40164
YangTzuHung <j25889651556@gmail.com>
2022-03-14 07:05:15 +0800

    202203140705

--- create_user.php
+++ create_user.php
@@ -19,46 +19,43 @@ if ($method == "OPTIONS") {
 // 引入檔案
 include_once 'config/DBconnect.php';
 include_once 'objects/user.php';
- 
+
 // get database connection (connect.php)
 $database = new Database();
 $db = $database->getConnection();
- 
+
 // instantiate product object
 $user = new User($db);
 
 // get posted data
 $data = json_decode(file_get_contents("php://input"));
- 
+
 // set product property values
 $user->firstname = $data->firstname;
 $user->lastname = $data->lastname;
 $user->email = $data->email;
 $user->password = $data->password;
- 
+$email_exists = $user->emailExists();
 // 檢查有無空值後將資料加到資料庫
-if(
-    !empty($user->firstname) &&
-    !empty($user->email) &&
-    !empty($user->password) &&
-    $user->create()
-){
- 
+
+if ($email_exists == true) {
+    // set response code
+    http_response_code(404);
+
+    // display message: user was created
+    echo json_encode(array("message" => "Email重複"));
+} elseif (!empty($user->firstname) && !empty($user->email) && !empty($user->password) && $user->create()) {
+
     // set response code
     http_response_code(200);
- 
+
     // display message: user was created
-    echo json_encode(array("message" => "User was created."));
-}
- 
-// message if unable to create user
-else{
- 
+    echo json_encode(array("message" => "註冊完成"));
+} else {
+
     // set response code
     http_response_code(400);
- 
+
     // display message: unable to create user
-    echo json_encode(array("message" => "Unable to create user."));
+    echo json_encode(array("message" => "註冊失敗"));
 }
-?>
- 
